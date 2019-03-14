c215f936588888c9faef72bbdb6edcc3c168fc6c
Varangian41 <jankovmilan1988@hotmail.com>
2019-03-14 17:41:59 +0100

    WIP actuator handler

--- wolk-nodes/add-sensor-reading.js
+++ wolk-nodes/add-sensor-reading.js
@@ -3,16 +3,17 @@ module.exports = RED => {
         RED.nodes.createNode(this, config);
         const context = this.context();
         const flow = context.flow;
+        this.reference = config.reference;
+        this.msgComplete = config.msgComplete;
+        this.timestamp = config.timestamp ? config.timestamp : false;
         this.on('input', msg => {
-            this.value = config.value || msg.payload;
-            this.reference = config.reference;
-            this.msgComplete = config.msgComplete;
-
+            this.value = config.value ? config.value : msg.payload;
             if (flow.connected) {
                 msg.payload = {
+                    type: 'sensor',
                     reference: this.reference,
                     topic: `readings/${flow.device.key}/${this.reference}`,
-                    payload: this.reference === 'ACL' ? [{data: `${this.value},${this.value},${this.value}`}] : [{data: this.value}]
+                    payload: this.timestamp ? [{utc: Date.now(), data: this.value.toString()}] : [{data: this.value.toString()}]
                 }
                 if (this.msgComplete) {
                     msg.complete = this.msgComplete;
--- wolk-nodes/set-actuator.js
+++ wolk-nodes/set-actuator.js
@@ -1,25 +1,31 @@
 module.exports = RED => {
-    function setActuator(config) {
+    function actuationHandler(config) {
         RED.nodes.createNode(this, config);
         const context = this.context();
         const flow = context.flow;
+        this.reference = config.reference;
+        this.returnValue = config.returnValue;
         this.on('input', msg => {
+            this.msgComplete = config.msgComplete ? config.msgComplete : false;
             this.value = config.value || msg.payload;
-            this.reference = config.reference;
 
             if (!flow.connected) {
                 throw new Error('Connect device to platform!');
             }
 
             msg.payload = {
+                type: 'actuator',
                 reference: this.reference,
                 topic: `actuators/status/${flow.device.key}/${this.reference}`,
-                payload: {status: "READY", value: JSON.parse(this.value).value}
+                payload: [{status: "READY", value: this.returnValue ? this.value.value : this.value}]
             }
-            msg.complete = true;
+
+            if (this.msgComplete) {
+                msg.complete = this.msgComplete;
+            }
 
             this.send(msg);
         });
     }
-    RED.nodes.registerType('setActuator', setActuator);
+    RED.nodes.registerType('actuationHandler', actuationHandler);
 }
